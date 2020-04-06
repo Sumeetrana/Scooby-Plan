e8b5a43d7f7db3cd467d57f0d59130e838a1b28d
Sumeetrana <sumeetsinh28@gmail.com>
2020-04-06 18:41:02 +0530

    Signup component added

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 
-class SignIn extends Component {
+class Signup extends Component {
     state = {
         email: '',
-        password: ''
+        password: '',
+        firstName: '',
+        lastName: ''
     }
     handleSubmit = (e) => {
         e.preventDefault();
@@ -19,17 +21,25 @@ class SignIn extends Component {
         return (
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
-                    <h5 className="grey-text text-darken-3">Sign In</h5>
+                    <h5 className="grey-text text-darken-3">Sign Up</h5>
                     <div className="input-field">
-                        <label>Email</label>
+                        <label htmlFor="firstName">First Name</label>
+                        <input type="text" id="firstName" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input type="text" id="lastName" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="email">Email</label>
                         <input type="email" id="email" onChange={this.handleChange} />
                     </div>
                     <div className="input-field">
-                        <label>Password</label>
+                        <label htmlFor="password">Password</label>
                         <input type="password" id="password" onChange={this.handleChange} />
                     </div>
                     <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Login</button>
+                        <button className="btn pink lighten-1 z-depth-0">Signup</button>
                     </div>
                 </form>
             </div>
@@ -37,4 +47,4 @@ class SignIn extends Component {
     }
 }
 
-export default SignIn
+export default Signup
