dd716cf59315925ad2d969d40d5b92971b67b030
Sumeetrana <sumeetsinh28@gmail.com>
2020-04-06 17:45:17 +0530

    Added ProjectSummary component and implement it in ProjectList component

--- src/components/projects/ListProject.js
+++ src/components/projects/ListProject.js
@@ -1,32 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ListProject = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project title</span>
-                    <p>Posted by Net Ninja</p>
-                    <p className="grey-text">3rd September, 3:00PM</p>
-                </div>
-                
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project title</span>
-                    <p>Posted by Net Ninja</p>
-                    <p className="grey-text">3rd September, 3:00PM</p>
-                </div>
-                
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project title</span>
-                    <p>Posted by Net Ninja</p>
-                    <p className="grey-text">3rd September, 3:00PM</p>
-                </div>
-                
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
