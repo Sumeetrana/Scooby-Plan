005460a33359f0ab26b7496b06abd983bdcdb381
Sumeetrana <sumeetsinh28@gmail.com>
2020-04-07 19:44:03 +0530

    Added redux thunk and async code using thunk

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { createProject } from '../../store/actions/projectActions';
 
 class CreateProject extends Component {
     state = {
@@ -7,7 +9,7 @@ class CreateProject extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);                
+        this.props.createProject(this.state)               
     }
     handleChange = (e) => {
         
@@ -37,4 +39,10 @@ class CreateProject extends Component {
     }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createProject: (project) => dispatch(createProject(project))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject)
