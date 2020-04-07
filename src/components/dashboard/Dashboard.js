55ba1efea43597058b94e3312654d2784f8a2692
Sumeetrana <sumeetsinh28@gmail.com>
2020-04-07 18:49:46 +0530

    Added dummy data

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import Notifications from './Notifications'
 import ProjectList from '../projects/ListProject'
 
 class DashBoard extends React.Component {
+    
+    
     render() {
+        // console.log(this.props);
+        const { projects } = this.props;
         return (
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6">
-                        <ProjectList />
+                        <ProjectList projects={projects} />
                     </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications />
@@ -21,4 +26,10 @@ class DashBoard extends React.Component {
     }
 }
 
-export default DashBoard;
+const mapStateToProps = (state) => {
+    return {
+        projects: state.project.projects
+    }
+}
+
+export default connect(mapStateToProps)(DashBoard);
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,9 @@
 const initState = {
+    projects: [
+        {id: 1, title: "title 1", content: "blah blah blah"},
+        {id: 2, title: "title 2", content: "blah blah blah"},
+        {id: 3, title: "title 3", content: "blah blah blah"}
+    ]
 }
 
 const projectReducer = (state = initState, action) => {
